3fbddf03804f478f60aa8bda5ee3d9fbc7ac83fd
Johnny Hausman <johnny@hausmanfamily.com>
2023-04-29 22:07:43 -0500

    [wip] refactor config/local.js

--- config/local.js
+++ config/local.js
@@ -1,18 +1,8 @@
 /*
  * notification_email
  */
-function env(envKey, defaultValue) {
-   if (typeof process.env[envKey] == "undefined") {
-      return defaultValue;
-   }
-   try {
-      return JSON.parse(process.env[envKey]);
-   } catch (e) {
-      console.log(e);
-      console.log(process.env[envKey]);
-      return process.env[envKey];
-   }
-}
+const AB = require("@digiserve/ab-utils");
+const env = AB.defaults.env;
 
 module.exports = {
    notification_email: {
@@ -81,22 +71,5 @@ module.exports = {
     * datastores:
     * Sails style DB connection settings
     */
-   datastores: {
-      appbuilder: {
-         adapter: "sails-mysql",
-         host: env("MYSQL_HOST", "db"),
-         port: env("MYSQL_PORT", 3306),
-         user: env("MYSQL_USER", "root"),
-         password: process.env.MYSQL_PASSWORD,
-         database: env("MYSQL_DBPREFIX", "appbuilder"),
-      },
-      site: {
-         adapter: "sails-mysql",
-         host: env("MYSQL_HOST", "db"),
-         port: env("MYSQL_PORT", 3306),
-         user: env("MYSQL_USER", "root"),
-         password: process.env.MYSQL_PASSWORD,
-         database: env("MYSQL_DBADMIN", "appbuilder-admin"),
-      },
-   },
+   datastores: AB.defaults.datastores(),
 };
